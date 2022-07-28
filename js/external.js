db7204f0deba4b27f4521f875b7cfc7c1d15c982
Will Armstrong <adamcarr@Wills-MBP-72022.localdomain>
2022-07-28 14:03:56 -0500

    notes and first actual push

---
+++ js/external.js
@@ -0,0 +1,12 @@
+"use strict"
+let color
+let myAge = 25
+console.log("My age is (myAge)")
+console.log("This is my external file")
+alert("Welcome to my website.")
+confirm("Click okay if you are a student")
+var isStudent = confirm("Click okay if you are a student please.")
+var favColor= prompt(message:"What is your favorite color?")
+var lastname = prompt(message:"Great favColor is my favorite color too!")
+
+console.log(firstName +, " " lastName)
---
+++ js/internal.js
@@ -0,0 +1,1 @@
+console.log("Hello from inline Java Script")
