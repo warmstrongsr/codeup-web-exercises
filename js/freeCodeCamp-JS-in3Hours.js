789e75367fa0ca540cb2aa79a6fa22294eb4206b
adamcarr <will.ea.armstrong@gmail.com>
2022-07-29 18:05:42 -0500

    Freecodecamp exercise work based on 3 hour video.  Completed 1 hr today.

---
+++ js/freeCodeCamp-JS-in3Hours.js
@@ -0,0 +1,20 @@
+console.log ("Hello from JS")
+
+var a;
+var b = 2;
+
+a =7;
+
+b = a;
+
+console.log (a)
+
+var a = 9;
+
+var a; = 5;
+var b; = 10;
+var c; = "I am a string"
+
+
+var sum = 10 + 10;
+console.log (sum)
