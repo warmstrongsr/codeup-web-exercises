3b3498b12941c9e04f40d7d4610a21954cd35582
adamcarr <will.ea.armstrong@gmail.com>
2022-07-30 23:05:26 -0500

    Freecodecamp exercise work based on 3 hour video.  Completed 1 hr today.

--- js/freeCodeCamp-JS-in3Hours.js
+++ js/freeCodeCamp-JS-in3Hours.js
@@ -1,20 +1,6 @@
 console.log ("Hello from JS")
-
-var a;
-var b = 2;
-
-a =7;
-
-b = a;
-
-console.log (a)
-
-var a = 9;
-
-var a; = 5;
-var b; = 10;
-var c; = "I am a string"
+//I will primarily be working on understanding concatenate.
+const sum = 10 + 10;
+//stopping at 26% of FCC JS
 
 
-var sum = 10 + 10;
-console.log (sum)
