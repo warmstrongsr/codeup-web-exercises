ff909a79c5870e0c0281a2f09102068eca19253e
adamcarr <will.ea.armstrong@gmail.com>
2022-08-03 16:41:09 -0500

    Freecodecamp exercise work based on 3 hour video.  Completed 1 hr today.

--- js/iterating.js
+++ js/iterating.js
@@ -1,2 +1,55 @@
-console.log("Hello from iterating");
+// "use strict";
+//
+// (function(){
+//     var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+//     console.log(shapes.length);
+//     console.log(shapes);
+//     console.log(shapes[1]);
+//     //loop through the array and the log the values
+//     for (var i = 0; 1 < shapes.length; i++) {
+//         console.log('The shape ar index ' + i + 'is:' + shapes[i]);
+//     }
+//     shapes.forEach(function (shape) {
+//         shape = shape + '- shape';
+//         console.log('Here is a lovely shape: ' + shape + '.');
+//     });
+// console.log(shapes);
+// })();
+
+var names = ['ryan', 'will', 'yogesh', 'vince', 'nicki', 'jordy'];
+
+console.log(names.length);
+
+console.log(naems[0]);
+console.log(naems[1]);
+console.log(naems[2]);
+console.log(naems[3]);
+
+for(let i =0; i < names.length; 1++){
+    console.log(names[i]);
+}
+
+names.forEach(function (name) {
+    console.log(name);
+})
+
+    function first(exampleArray) {
+        console.log(exampleArray[0]);
+    }
+        first([1,2,3,4,5]);
+
+    function second(exampleArray) {
+        console.log(exampleArray[1]);
+    }
+        second([1,2,3,4,5]);
+
+    function last(exampleArray) {
+        console.log(exampleArray[0]);
+    }
+        last([1,2,3,4,5])
+
+})()
+
+
+
 
