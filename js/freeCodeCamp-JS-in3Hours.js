8857b7b5de941082c42f3800aaa2facb1b0185a9
Will Armstrong <will.ea.armstrong@gmail.com>
2022-08-06 21:11:42 -0500

    Pushing from VS Code while studying.

--- js/freeCodeCamp-JS-in3Hours.js
+++ js/freeCodeCamp-JS-in3Hours.js
@@ -1,6 +1,66 @@
 console.log ("Hello from JS")
 //I will primarily be working on understanding concatenate.
 const sum = 10 + 10;
+console.log(sum);
+
+var myStr = "FirstLine\n\t\SecondLine\ \nThirdLine";
+console.log(myStr);
+
+//example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "FirstLine\nSecondLine\r\nThirdLine\nFourthLine\nFifthLine\r\nSixthLine";
+console.log(myStr);
+
+var myStr = `I am a \"double quoted\" string inside \"double quotes\".`;
+console.log(myStr);
+
+var myStr = `<a href="http://www.example.com" target="_blank">Link</a>`;
+console.log(myStr);
+
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end."
+console.log(myStr);
+
+var myStr = "This is the first sentence. "
+myStr += "\nThis is the second sentence.";
+console.log(myStr);
+
+var myName = "Will";
+var myStr = "My name is " + myName + " and I am well!";
+console.log(myStr);
+
+var someAdjective = "worthwhile.";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
+console.log(myStr);
+
+
+var firstNameLength = 0;
+var firstName = "Taz";
+
+firstNameLength = firstName.length;
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+console.log(firstNameLength);
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+
+console.log(lastName);
+console.log(lastNameLength);
+
+console.log(myStr);
+console.log(myStr);
+console.log(myStr);
 
 
 
