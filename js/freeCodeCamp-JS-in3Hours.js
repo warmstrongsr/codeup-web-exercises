fe43c0641b008108472207dfac537e1a8b53033c
Will Armstrong <will.ea.armstrong@gmail.com>
2022-08-07 19:37:21 -0500

    js coding practice

--- js/freeCodeCamp-JS-in3Hours.js
+++ js/freeCodeCamp-JS-in3Hours.js
@@ -1,7 +1,7 @@
 console.log ("Hello from JS")
 //I will primarily be working on understanding concatenate.
-const sum = 10 + 10;
-console.log(sum);
+const  summer = 10 + 10;
+console.log(summer);
 
 var myStr = "FirstLine\n\t\SecondLine\ \nThirdLine";
 console.log(myStr);
@@ -154,5 +154,108 @@ function fun2() {
 fun1();
 fun2();
 
+function ourReusableFunction() {
+    console.log("HeLlO WoRlD");
+}
+ourReusableFunction();
 
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
 
+//Example
+
+function ourFunctionWithArgs(alpha, bravo) {
+    console.log(alpha + bravo);
+}
+ourFunctionWithArgs(10, 5);
+
+function functionWithArguments(a, b, c, d, e, fg){
+    console.log(a + b  * c + d % e / fg);
+}
+functionWithArguments(10, 100, 1, 2, 5, 1);
+// self break
+
+function alphaFunction(alpha, bravo, charlie, delta) {
+    console.log((alpha + bravo + charlie) / delta);
+}
+alphaFunction(1, 2, 3, 4);
+
+//One more blind to test concepts
+
+function bravoFunction(a,b,c,d,e,f,g) {
+    console.log(a+b+c+d+e+f+g);
+}
+bravoFunction(10,10,10,10,10,10,10); //nailed it! moving on...
+
+var a = "I come first";
+var b = " and I come second.";
+    console.log(a + b);
+
+function areaFucntion(c, d, e, f, a, b, c) {
+    console.log((a + b + c) / d * 2 - 10 + e * f);
+}
+areaFucntion(1,2,2,2,2,2,10); //based on Order of Ops the correct answer logs 8
+
+// Break ******************* Break
+
+var myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5;
+    // Assign 5 to oopsGlobal Here
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+fun1();
+fun2();
+
+function myLocalScope() {
+    var myVar = 5;
+    console.log(myVar);
+}
+myLocalScope();
+
+// console.log(myVar); //error my var is not defined b/c its locally in the function
+
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+    var outerWear = "sweater";
+    return outerWear;
+}
+console.log(myOutfit());
+console.log(outerWear);
+
+function minusSeven(num) {
+    return num - 7;
+}
+
+console.log(minusSeven(10));
+
+function timesFive(num){
+    return num * 5;
+}
+console.log(timesFive(5));
+
+//example
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+console.log(addThree());
+
+function addFive() {
+    sum += 5;
+}
+console.log(addFive());
