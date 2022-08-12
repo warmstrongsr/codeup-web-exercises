aa31539233a8ea6c56c48552365d9b176ffd9871
Will Armstrong <will.ea.armstrong@gmail.com>
2022-08-11 19:19:46 -0500

    80% freeCodeCamp

--- js/freeCodeCamp-JS-in3Hours.js
+++ js/freeCodeCamp-JS-in3Hours.js
@@ -396,3 +396,113 @@ if(strokes == 1) {
 }
 
 golfScore(5, 4);
+
+//switch of stuff **************************************************************************
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+    switch(val){
+    case "a":
+    answer= "apple";
+    break;
+    case "b":
+    answer="bird";
+    break;
+    case "c":
+    answer="cat";
+    break;
+    default:
+    answer="stuff";
+    break;
+
+    }
+    
+  
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(5);
+
+//Break*******
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+ switch(val) {
+   case "bob":
+    answer="Marley";
+     break;
+  case 42:
+      answer="The Answer";
+      break;
+  case 1:
+      answer="There is no #1";
+      break;
+  case 99:
+      answer="Missed me by this much!";
+      break;
+    case 7:
+      answer="Ate Nine";
+      break;
+ }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+//Break****************************
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a<0||b<0){
+  return undefined;
+}
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+//Break
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch(card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+  count--;
+  break;
+}
+  let holdbet='Hold'
+if (count > 0) {holdbet = 'Bet'
+}
+  return count + " " + holdbet;
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+//HOLY *******************************************************************************************************
+
